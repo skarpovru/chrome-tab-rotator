9a9e378214f908770cb29053a4633dd812ef8f0c
Stanislav Karpov <stanislav.karpov@palas.de>
2024-08-22 02:56:21 +0200

    Added first tabs rotation logic implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,143 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, ReactiveFormsModule],
 })
-export class AppComponent {
-  title = 'chrome-tabs-rotator';
+export class AppComponent implements OnInit {
+  configForm: FormGroup;
+  dataSource: FormGroup[] = [];
+  isRotating = false; // Add this variable
+
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) {
+    this.configForm = this.fb.group({
+      pages: this.fb.array([]),
+    });
+
+    this.dataSource = (this.configForm.get('pages') as FormArray)
+      .controls as FormGroup[];
+  }
+
+  ngOnInit() {
+    this.displayCurrentConfig();
+    this.listenForRotationState();
+    this.queryRotationState();
+  }
+
+  get pages(): FormArray {
+    return this.configForm.get('pages') as FormArray;
+  }
+
+  displayCurrentConfig() {
+    chrome.storage.local.get('pages', (result) => {
+      const pages = result['pages'] || [];
+
+      if (pages.length === 0) {
+        // Start by default with one empty page
+        this.addNewPage();
+      } else {
+        this.pages.clear();
+        pages.forEach((page: any) => {
+          this.pages.push(
+            this.fb.group({
+              url: [page.url, Validators.required],
+              delay: [page.delay, Validators.required],
+              reloadInterval: [page.reloadInterval, Validators.required],
+            })
+          );
+        });
+      }
+
+      this.updateDataSource();
+    });
+  }
+
+  addPage() {
+    this.addNewPage();
+    this.updateDataSource();
+    console.log('Added page', this.pages.controls);
+  }
+
+  deletePage(index: number) {
+    this.pages.removeAt(index);
+    this.updateDataSource();
+  }
+
+  saveConfig(event: Event) {
+    event.preventDefault();
+    if (this.configForm.invalid) {
+      alert('Please fill in all fields.');
+      return;
+    }
+
+    const pages = this.configForm.value.pages;
+    chrome.storage.local.set({ pages }, () => {
+      console.log('Configuration saved');
+      this.displayCurrentConfig();
+    });
+  }
+
+  startRotation() {
+    chrome.runtime.sendMessage({ action: 'rotateTabs' });
+    this.isRotating = true; // Set to true when rotation starts
+  }
+
+  stopRotation() {
+    chrome.runtime.sendMessage({ action: 'stopRotation' });
+    this.isRotating = false; // Set to false when rotation stops
+  }
+
+  private addNewPage() {
+    this.pages.push(
+      this.fb.group({
+        url: ['', Validators.required],
+        delay: ['', Validators.required],
+        reloadInterval: ['', Validators.required],
+      })
+    );
+  }
+
+  private updateDataSource() {
+    this.dataSource = this.pages.controls as FormGroup[];
+    this.cdr.detectChanges();
+  }
+
+  private queryRotationState() {
+    chrome.runtime.sendMessage({ action: 'getRotationState' }, (response) => {
+      this.isRotating = response.isRotating;
+      this.cdr.detectChanges();
+    });
+  }
+
+  getFormControl(page: FormGroup, controlName: string): FormControl {
+    return page.get(controlName) as FormControl;
+  }
+
+  private listenForRotationState() {
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (message.action === 'rotationState') {
+        this.isRotating = message.isRotating;
+        this.cdr.detectChanges();
+      }
+    });
+  }
 }
--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,173 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.webNavigation.onCompleted.addListener(
-    () => {
-      console.log('Check if action is enabled');
-      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
-        if (id) {
-          console.log('Disabling action for tab112', id);
-          chrome.action.disable(id);
-        }
-      });
-    },
-    { url: [{ hostContains: 'google.com' }] }
-  );
+let pages: { url: string; delay: number; reloadInterval: number }[] = [];
+const maxRetries = 1;
+let tabIds: number[] = [];
+let currentIndex = 0;
+let reloadTimers: (number | ReturnType<typeof setInterval>)[] = [];
+let retryCount = 0;
+let rotationTimeout: ReturnType<typeof setTimeout>;
+let isRotating = false;
+
+function rotateTabs() {
+  console.log('rotateTabs called, isRotating:', isRotating);
+  if (!isRotating) {
+    console.error('Rotation is not active.');
+    return;
+  }
+
+  chrome.storage.local.get('pages', (result) => {
+    pages = result['pages'] || [];
+
+    if (tabIds.length === 0) {
+      console.error('No tabs available for rotation.');
+      isRotating = false;
+      chrome.runtime.sendMessage({ action: 'rotationState', isRotating }); // Send rotation state
+      return;
+    }
+
+    console.log('rotateTabs called', tabIds[currentIndex]);
+    if (tabIds[currentIndex] !== undefined) {
+      chrome.tabs.update(tabIds[currentIndex], { active: true });
+      const currentDelay = pages[currentIndex].delay * 1000;
+      currentIndex = (currentIndex + 1) % pages.length;
+      console.info('rotationTimeout currentDelay:', currentDelay);
+      rotationTimeout = setTimeout(rotateTabs, currentDelay);
+    } else {
+      console.error('Invalid tab ID:', tabIds[currentIndex]);
+      currentIndex = (currentIndex + 1) % pages.length;
+      rotateTabs();
+    }
+  });
+}
+
+function stopRotation() {
+  console.log('stopRotation called');
+  clearTimeout(rotationTimeout);
+  isRotating = false;
+  chrome.runtime.sendMessage({ action: 'rotationState', isRotating }); // Send rotation state
+  reloadTimers.forEach((timer) => clearInterval(timer as number));
+  reloadTimers = [];
+  tabIds.forEach((tabId) => {
+    if (tabId !== undefined) {
+      chrome.tabs.remove(tabId);
+    }
+  });
+  tabIds = [];
+  currentIndex = 0;
+  retryCount = 0;
+}
+
+function createTabs(callback: () => void) {
+  let createdTabs = 0;
+  pages.forEach((page, index) => {
+    chrome.tabs.create({ url: page.url, active: index === 0 }, (tab) => {
+      console.log('Tab created:', tab);
+      tabIds[index] = tab.id!;
+      createdTabs++;
+      if (createdTabs === pages.length) {
+        callback();
+      }
+    });
+  });
+}
+
+function startReloadTimers() {
+  pages.forEach((page, index) => {
+    if (page.reloadInterval) {
+      reloadTimers[index] = setInterval(() => {
+        chrome.tabs.reload(tabIds[index]);
+      }, page.reloadInterval * 1000);
+    }
+  });
+}
+
+function handleError(tabId: number, errorUrl: string) {
+  console.info('handleError tabId, errorUrl', tabId, errorUrl);
+
+  const pageIndex = tabIds.indexOf(tabId);
+  if (
+    pageIndex !== -1 &&
+    pages[pageIndex] &&
+    pages[pageIndex].url === errorUrl
+  ) {
+    if (retryCount < maxRetries) {
+      retryCount++;
+      chrome.tabs.update(tabId, { url: errorUrl });
+    } else {
+      retryCount = 0;
+      clearInterval(reloadTimers[pageIndex] as number);
+      reloadTimers.splice(pageIndex, 1);
+      tabIds.splice(pageIndex, 1);
+      pages.splice(pageIndex, 1);
+      console.info('Tab removed from rotation due to repeated errors:', tabId);
+      if (currentIndex >= tabIds.length) {
+        currentIndex = 0;
+      }
+      chrome.tabs.remove(tabId);
+    }
+  } else {
+    console.error('Invalid tabId or page not found for tabId:', tabId);
+  }
+}
+
+if (chrome.webNavigation && chrome.webNavigation.onErrorOccurred) {
+  chrome.webNavigation.onErrorOccurred.addListener((details) => {
+    handleError(details.tabId, details.url);
+  });
+} else {
+  console.error('webNavigation API is not available.');
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log('Message received:', message, isRotating);
+  if (message.action === 'rotateTabs') {
+    if (!isRotating) {
+      console.log('Starting rotation from message listener');
+      isRotating = true;
+      chrome.runtime.sendMessage({ action: 'rotationState', isRotating }); // Send rotation state
+      if (tabIds.length === 0) {
+        createTabs(() => {
+          rotateTabs();
+          startReloadTimers();
+        });
+      } else {
+        rotateTabs();
+        startReloadTimers();
+      }
+    }
+  } else if (message.action === 'stopRotation') {
+    stopRotation();
+  } else if (message.action === 'getRotationState') {
+    sendResponse({ isRotating });
+  }
 });
+
+chrome.action.onClicked.addListener(() => {
+  if (!isRotating) {
+    console.log('Starting rotation from action button');
+    isRotating = true;
+    chrome.runtime.sendMessage({ action: 'rotationState', isRotating }); // Send rotation state
+    if (tabIds.length === 0) {
+      createTabs(() => {
+        rotateTabs();
+        startReloadTimers();
+      });
+    } else {
+      rotateTabs();
+      startReloadTimers();
+    }
+  }
+});
+
+if (chrome.storage && chrome.storage.local) {
+  chrome.storage.local.get('pages', (result) => {
+    pages = result['pages'] || [];
+  });
+
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (namespace === 'local' && changes['pages']) {
+      pages = changes['pages'].newValue;
+    }
+  });
+} else {
+  console.error('Chrome Storage API is not available.');
+}
